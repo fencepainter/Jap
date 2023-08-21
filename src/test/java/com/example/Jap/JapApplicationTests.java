846577ca0ca951e47995c233d0c52c10ff5141cd
fencepainter <fencepainter2@gmail.com>
2023-08-21 21:56:03 +0200

    first commit

---
+++ src/test/java/com/example/Jap/JapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
